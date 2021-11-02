74cd290d90c5b0343b95c7c121b4891d7381a53c
瀬戸博之 <setohiroyuki@MacBookAir.local>
2021-11-02 19:45:32 +0900

    first commit

---
+++ src/main/kotlin/com/hiswing/archunitsample/ArchunitsampleApplication.kt
@@ -0,0 +1,11 @@
+package com.hiswing.archunitsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchunitsampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchunitsampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/hiswing/archunitsample/ArchunitsampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hiswing.archunitsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchunitsampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
