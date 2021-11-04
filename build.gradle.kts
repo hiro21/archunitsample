5d4115efa7e1c0be747577d63dfe58bd57922045
瀬戸博之 <setohiroyuki@MacBookAir.local>
2021-11-04 19:29:26 +0900

    add ArchUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.tngtech.archunit:archunit:0.22.0")
 }
 
 tasks.withType<KotlinCompile> {
