2c394c86bdba3377e065866033c661ea27c86fe8
hiroyuki-seto <hiswing8@gmail.com>
2022-03-03 19:50:42 +0900

    archunitのテストを追加してみる。

--- src/test/kotlin/com/hiswing/archunitsample/ArchTest.kt
+++ src/test/kotlin/com/hiswing/archunitsample/ArchTest.kt
@@ -2,6 +2,8 @@ package com.hiswing.archunitsample
 
 import com.tngtech.archunit.core.importer.ClassFileImporter
 import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -13,6 +15,13 @@ class ArchTest {
             val doNotIncludeTest = ImportOption.DoNotIncludeTests()
             val importedClasses = ClassFileImporter().withImportOption(doNotIncludeTest).importPackages("com.hiswing.archunitsample")
             println("testPrint:$importedClasses")
+        }
+        @Test
+        fun `usecaseはapi配下以外に依存しない`() {
+            val importedClasses = ClassFileImporter().importPackages("com.hiswing.archunitsample.api")
+            val rule: ArchRule = noClasses().that().resideInAPackage("..usecase..")
+                .should().dependOnClassesThat().resideInAPackage("..api..")
+            rule.check(importedClasses)
         }
     }
 }
