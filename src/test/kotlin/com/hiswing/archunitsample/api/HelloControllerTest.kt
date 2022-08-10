4252368f2c302b91b1822e268b60a76c648cd935
hiroyuki-seto <hiswing8@gmail.com>
2022-08-10 19:32:40 +0900

    テストをメンテナンス

--- src/test/kotlin/com/hiswing/archunitsample/api/HelloControllerTest.kt
+++ src/test/kotlin/com/hiswing/archunitsample/api/HelloControllerTest.kt
@@ -1,16 +1,18 @@
 package com.hiswing.archunitsample.api
 
+import com.hiswing.archunitsample.domain.PrintTimeJapan
 import com.hiswing.archunitsample.usecase.HelloUsecase
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class HelloControllerTest {
+    private val printTimeJapan = PrintTimeJapan()
     @Nested
     inner class `hello APIを呼び出す` {
         @Test
         fun `helloエンドポイントにアクセスされたらhelloを返す`() {
-            val usecase = HelloUsecase()
+            val usecase = HelloUsecase(printTimeJapan)
             val controller = HelloController(usecase)
             val result: String = controller.hello()
             assertEquals("hello", result)
